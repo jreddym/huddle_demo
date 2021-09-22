b5d175cfa6f80027c02c52c3c5e8242b8c5b2333
Jagadeeswara Reddy <jagadeer@opentext.com>
2021-09-22 13:58:05 +0530

    update proj name

---
+++ src/components/styles/Flex.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > div,
+  & > ul {
+    flex: 1;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    text-align: center;
+    flex-direction: column;
+  }
+`;
